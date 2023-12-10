12f0dc8c89e60b187232d5ada8bf1e83bc779f63
Jahid Hasan <jahidhasanofficial23@gmail.com>
2023-12-10 23:51:38 +0600

    add admin panel

---
+++ resources/views/backend/layout/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Admin</title>
+    <!-- plugins:css -->
+   @include('backend.component.style')
+  </head>
+  <body>
+      <!-- partial:partials/_navbar.html -->
+    @include('backend.component.header')
+      <!-- partial -->
+      <div class="container-fluid page-body-wrapper">
+        <!-- partial:partials/_sidebar.html -->
+        @include('backend.component.sidebar')
+        <!-- partial -->
+        <div class="main-panel">
+          @yield('content')
+          <!-- content-wrapper ends -->
+          <!-- partial:partials/_footer.html -->
+         @include('backend.component.footer')
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+   @include('backend.component.script')
+    <!-- End custom js for this page -->
+  </body>
+</html>
